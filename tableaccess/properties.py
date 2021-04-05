45a4fd0732413b110600392bf792970958f612f6
tsharma <tsharma@cloudera.com>
2021-04-05 14:33:42 +0530

    missing property update

--- tableaccess/properties.py
+++ tableaccess/properties.py
@@ -4,6 +4,7 @@ import os
 
 class Properties:
     sqlite_db_path = os.path.join(os.path.dirname(__file__), 'RunQueue.db')
+    cost_tracker_sqlite_db = os.path.join(os.path.dirname(__file__), 'CostTracker.db')
 
 
 class RunStates(enum.Enum):
@@ -12,3 +13,9 @@ class RunStates(enum.Enum):
     FAILED = "FAILED"
     SUCCESS = "SUCCESS"
     INITIATED = "INITIATED"
+
+
+class CloudType(enum.Enum):
+    aws = "aws"
+    azure = "azure"
+    gcp = "gcp"
